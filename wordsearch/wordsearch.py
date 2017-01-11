93d718bf4fef013453b89d9d8787c6d973ac2eb1
alex-dr <alex@datarobot.com>
2017-01-11 13:51:18 -0500

    CLI!

--- wordsearch/wordsearch.py
+++ wordsearch/wordsearch.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import argparse
 from random import choice
 from string import rstrip
 import sys
+
+
+LONGEST_WORD_LENGTH = 22
 
 
 def generate_random_board(n=9):
@@ -17,6 +21,13 @@ def print_board(board):
     print('---' + '--'*len(board[0]))
 
 
+def print_words(words):
+    def print_word(word):
+        print(''.join(zip(*word)[0]))
+    for word in words:
+        print_word(word)
+
+
 def read_board(content):
     """Read a board that was loaded with file.readlines()."""
     return map(lambda x: x.rstrip().split(' '), content)
@@ -29,6 +40,10 @@ def longest_string(board, row, col, drow, dcol, acc):
     """
     if row < 0 or col < 0:
         return acc
+
+    if len(acc) > LONGEST_WORD_LENGTH:
+        return acc
+
     try:
         acc.append((board[row][col], row, col))
     except IndexError:
@@ -37,8 +52,8 @@ def longest_string(board, row, col, drow, dcol, acc):
     return longest_string(board, row + drow, col + dcol, drow, dcol, acc)
 
 
-def find_longest_word(valid_words, candidate):
-    if not candidate:
+def find_longest_word(valid_words, candidate, min_word_length):
+    if len(candidate) < min_word_length:
         return None
 
     test_string = ''.join(x[0] for x in candidate)
@@ -47,7 +62,7 @@ def find_longest_word(valid_words, candidate):
         return candidate
 
     else:
-        return find_longest_word(valid_words, candidate[:-1])
+        return find_longest_word(valid_words, candidate[:-1], min_word_length)
 
 
 def add_word_to_board(word, new_board):
@@ -55,7 +70,7 @@ def add_word_to_board(word, new_board):
         new_board[row][col] = char
 
 
-def update_board(valid_words, board, new_board, row, col):
+def update_board(valid_words, board, new_board, row, col, min_word_length):
     """Update the new board with any words found at this position.
 
     Parameters
@@ -70,41 +85,64 @@ def update_board(valid_words, board, new_board, row, col):
         Row index (first index into boards)
     col: int
         Column index (second index)
+    min_word_length: int
+        Smallest word to find
 
     Side Effects
     ------------
     Updates new_board with new words.
     """
+    words = []
     for drow in [-1, 0, 1]:
         for dcol in [-1, 0, 1]:
             if not (drow == 0 and dcol == 0):
                 candidate = longest_string(board, row, col, drow, dcol, [])
-                word = find_longest_word(valid_words, candidate)
-                if word is not None and len(word) >= 4:
-                    print('found word: {}'.format(''.join(x[0] for x in word)))
+                word = find_longest_word(valid_words, candidate, min_word_length)
+                if word is not None and len(word) >= min_word_length:
+                    words.append(word)
                     add_word_to_board(word, new_board)
+    return words
 
 
-def solve_board(valid_words, board):
+def solve_board(valid_words, board, min_word_length):
     width = len(board[0])
     height = len(board)
 
     new_board = [[' ' for _ in range(width)] for _ in range(height)]
 
+    words = []
     for row in range(height):
         for col in range(width):
-            update_board(valid_words, board, new_board, row, col)
+            words += update_board(valid_words, board, new_board, row, col, min_word_length)
 
-    return new_board
+    return new_board, words
 
 
-def main(valid_words, *args):
-    board = generate_random_board(n=15)
-    print_board(board)
+def main(valid_words, args):
+    parser = argparse.ArgumentParser(
+        description='Generate a random wordsearch and solve it')
 
-    solved_board = solve_board(valid_words, board)
+    parser.add_argument('--board-size', type=int, default=15,
+                        help='Number of rows and columns')
+    parser.add_argument('--min-word-length', type=int, default=4,
+                        help='Smallest words to find')
+    parser.add_argument('--just-show-words', action='store_true',
+                        help='Only return found words')
 
-    print_board(solved_board)
+    args = parser.parse_args()
+
+    board = generate_random_board(n=args.board_size)
+
+    if not args.just_show_words:
+        print_board(board)
+
+    solved_board, words = solve_board(valid_words, board, args.min_word_length)
+
+    if args.just_show_words:
+        print_words(words)
+    else:
+        print_board(solved_board)
+        print_words(words)
 
 
 if __name__ == '__main__':
